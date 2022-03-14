a3785524a355103127b29d3f7e354eae3e5b174b
K009 <spidahas@gmail.com>
2022-03-14 10:23:44 +0100

    Added player and basic movement.

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,33 @@
-console.log("Hello world!");
+let playerContainerElement = document.querySelector(".player");
+let moveUnit = 100;
+window.addEventListener('load', () => {
+    playerContainerElement.style.position = 'absolute';
+    playerContainerElement.style.marginLeft = 'auto';
+    playerContainerElement.style.marginRight = 'auto';
+    playerContainerElement.style.left = '0';
+    playerContainerElement.style.right = '0';
+    playerContainerElement.style.top = '0';
+});
+// FFR - for future refactor
+window.addEventListener('keydown', (e) => {
+    let width = window.innerWidth;
+    let leftOfPlayer = parseInt(playerContainerElement.style.left);
+    let playerWidth = 150; // value taken from css file 
+    let playerCenter = playerWidth / 2 + leftOfPlayer;
+    if (playerCenter > width / 2) {
+        playerContainerElement.style.left = parseInt(playerContainerElement.style.left) - moveUnit + "px";
+    }
+    else if (playerCenter < -(width / 2)) {
+        playerContainerElement.style.left = parseInt(playerContainerElement.style.left) + moveUnit + "px";
+    }
+    else {
+        switch (e.key) {
+            case 'ArrowLeft':
+                playerContainerElement.style.left = parseInt(playerContainerElement.style.left) - moveUnit + "px";
+                break;
+            case "ArrowRight":
+                playerContainerElement.style.left = parseInt(playerContainerElement.style.left) + moveUnit + "px";
+                break;
+        }
+    }
+});
